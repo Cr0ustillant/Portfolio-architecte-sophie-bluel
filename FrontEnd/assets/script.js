1d3ba739f9cfb51b03d8baf435d12df79d9f866f
Jerôme Duprez <jeromeduprez@outlook.fr>
2024-01-12 21:21:44 +0100

    filtrage fin

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -4,53 +4,65 @@ const buttonAll = document.createElement("button");
 const btns = document.createElement("div");
 btns.appendChild(buttonAll);
 portfolio.appendChild(btns);
-btns.className = 'btns';
+btns.className = "btns";
 buttonAll.innerHTML = "Tous";
-gallery.insertAdjacentElement("beforebegin" , btns);
+gallery.insertAdjacentElement("beforebegin", btns);
 
 async function works() {
-    const r = await fetch("http://localhost:5678/api/works");
-    const listWorks = await r.json();
-    console.log(listWorks);
-    
-    listWorks.forEach(element => {
-        const figureBalise = document.createElement("figure");
-        const imgBalise = document.createElement("img");
-        const figcaptionBalise = document.createElement("figcaption");
+  const v = await fetch("http://localhost:5678/api/works");
+  const listWorks = await v.json();
+  console.log(listWorks);
 
-        gallery.appendChild(figureBalise);
-        figureBalise.appendChild(imgBalise);
-        figureBalise.appendChild(figcaptionBalise);
+  listWorks.forEach((element) => {
+    const figureBalise = document.createElement("figure");
+    const imgBalise = document.createElement("img");
+    const figcaptionBalise = document.createElement("figcaption");
 
-        imgBalise.src = element.imageUrl;
-        imgBalise.alt = element.title;
-        figcaptionBalise.innerHTML = element.title;
-        figureBalise.classList = element.category.name;
-    }); 
+    gallery.appendChild(figureBalise);
+    figureBalise.appendChild(imgBalise);
+    figureBalise.appendChild(figcaptionBalise);
+
+    imgBalise.src = element.imageUrl;
+    imgBalise.alt = element.title;
+    figcaptionBalise.innerHTML = element.title;
+    figureBalise.id = element.categoryId;
+  });
 }
 
 async function categories() {
-    const r = await fetch("http://localhost:5678/api/categories");
-    const listCategories = await r.json();
-    console.log(listCategories);
-    const btns = document.querySelector(".btns");
-    let figureBalise = document.querySelector("#gallery figure");
-    listCategories.forEach(element => {
-        const buttonBalise = document.createElement("button");
-        btns.appendChild(buttonBalise);
-        
-        buttonBalise.innerHTML = element.name;
-        buttonBalise.className = element.name;
-        
+  const r = await fetch("http://localhost:5678/api/categories");
+  const listCategories = await r.json();
 
+  console.log(listCategories);
 
-        buttonBalise.addEventListener("click", function() {
-            listCategories.forEach(element => {
-                figureBalise.className == buttonBalise.className
-            });
-        });
+  const btns = document.querySelector(".btns");
+
+  listCategories.forEach((element) => {
+    const buttonBalise = document.createElement("button");
+    btns.appendChild(buttonBalise);
+    buttonBalise.innerHTML = element.name;
+    buttonBalise.id = element.id;
+  });
+
+  buttonBalise = document.querySelectorAll(".btns button");
+
+  buttonBalise.forEach((button) => {
+    button.addEventListener("click", function comparaisonId() {
+      let figures = document.querySelectorAll(".gallery figure");
+      const choixUtilisateur = this.id;
+
+      figures.forEach((figure) => {
+        if (choixUtilisateur === figure.id || "") {
+          figure.style.display = "block";
+        } else if (choixUtilisateur === "") {
+          figure.style.display = "block";
+        } else {
+          figure.style.display = "none";
+        }
+        console.log(choixUtilisateur, "ok");
+      });
     });
+  });
 }
-
 works();
 categories();

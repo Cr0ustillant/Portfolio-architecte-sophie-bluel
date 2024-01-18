b70f654f748df9d1e72b6cd2505ce1221e3e7170
Jerôme Duprez <jeromeduprez@outlook.fr>
2024-01-18 21:55:22 +0100

    login verification

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,20 +1,49 @@
 const btnConnexion = document.getElementById("btn-connexion");
 
-btnConnexion.addEventListener("click" , async (event) => {
+async function login() {
+  btnConnexion.addEventListener("click", async (event) => {
     event.preventDefault();
-    const  email = document.getElementById('email').value;
-    const  password = document.getElementById('password').value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
     console.log(email, "ok");
     console.log(password, "ok");
-    const r = await fetch('http://localhost:5678/api/users/login',{
-        method: "POST" ,
-        headers: {   
-            'Content-Type':'application/json'
-        },
-        body : JSON.stringify({email,password})
+    const r = await fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({ email, password }),
     });
 
     const passwordAndEmail = await r.json();
     console.log(passwordAndEmail, "ok");
-    
+
+    localStorage.setItem("token", "");
+  });
+}
+
+// Vérification du champ de saisie
+
+let emailValue = document.getElementById("email");
+let passwordValue = document.getElementById("password");
+
+function verification(balise) {
+  if (balise.value === "") {
+    balise.classList.add("error");
+    alert("Error !!!");
+  } else {
+    balise.classList.remove("error");
+    window.location.href = "/index.html";
+  }
+}
+
+const form = document.querySelector("form");
+
+btnConnexion.addEventListener("click", (event) => {
+  event.preventDefault();
+  verification(emailValue);
+  verification(passwordValue);
+  console.log(btnConnexion, "ok");
 });
+
+login();
